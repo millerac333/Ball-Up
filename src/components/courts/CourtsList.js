da488a8138952044ee9bc647728b0a4449429b9c
Aaron Miller <millerac333@gmail.com>
2018-08-01 20:34:20 -0500

    began working with courts components

--- src/components/courts/CourtsList.js
+++ src/components/courts/CourtsList.js
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
 
-export default class Hello extends Component {
+export default class CourtsList extends Component {
   render() {
     return (
-      <React.Fragment>
-        <h1>Hello World</h1>
-      </React.Fragment>
+      <section className="courts">
+        {this.props.courts.map(courts => (
+          <div key={courts.id} className="card">
+            <div className="card-body">
+              <h4 className="card-courts courtName">{courts.name}</h4>
+              <h4 className="card-courts courtAddress">{courts.address}</h4>
+              <h4 className="card-courts courtHours">{courts.hours}</h4>
+              <h4 className="card-courts courtSize">{courts.size}</h4>
+              {/* <button
+                type="button"
+                className="card-courts court!id"
+                onClick={<GameForm />}
+              >
+                Add New Game {courts.id}
+              </button> */}
+            </div>
+          </div>
+        ))}
+      </section>
     );
   }
 }
